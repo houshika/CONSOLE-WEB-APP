c9476512f486a7e3c22b90258622c69d4fd3d8b8
houshika <93131094+houshika@users.noreply.github.com>
2022-07-10 15:32:46 +0530

    Add files via upload

---
+++ CONSOLE WEB APP.js
@@ -0,0 +1,82 @@
+
+//Replase
+ var ramdom = "If you need help just Go to 👉 https://www.google.com/search"   
+
+ console.log(ramdom)
+
+ console.log("Opps 😊😊 Wrong link")
+ 
+ var replase = ramdom.replace("https://www.google.com/search","https://code.visualstudio.com/docs")
+ 
+ console.log(replase)
+
+ console.log("This is the link")
+ 
+ //concat
+ var frist_name = "My name is Houshi";
+ 
+ var last_name = " sivakumar"
+ 
+ var full_name = frist_name.concat(last_name)
+ 
+ console.log(full_name)
+
+ //length
+ var age = "jhdyhfgjkh"
+ 
+ var length = age.length;
+
+ //none
+var sentence = "I am "
+
+ var my_age = sentence.concat(length)
+ 
+ console.log(my_age)
+ 
+ //Math
+ var square_root =  Math.sqrt(536)
+
+ var text = "Did you know that the square root of 536 is ".concat(square_root)
+
+ console.log(text)
+
+ var max = Math.max(12, 64, 56, 7654,  75, 66, 987, 68, 76)
+
+ var text2 ="The biggst number in this bunch 12, 64, 56, 7654,  75, 66, 987, 68, 76 is ".concat(max)
+ 
+ console.log(text2)
+
+ var min = Math.min(12, 64, 56, 7654, 75, 66, 987, 68, 76, 3, 11)
+
+ var text3 ="The smallest number in this bunch 12, 64, 56, 7654, 75, 66, 987, 68, 76, 3, 11 is ".concat(min)
+
+ console.log(text3)
+
+ var num1_plus = 2543030046240
+
+ var num2_plus = 54000000000000
+
+ var add = num1_plus + num2_plus
+
+ var text4 = "2543030046240 + 54000000000000 is ".concat(add)
+ 
+ console.log(text4)
+
+ var num1_minus = 776697543500000058653
+
+ var num2_minus = 457379540006347654023654
+
+ var minus = num2_minus - num1_minus
+
+ var text5 = "457379540006347654023654 - 776697543500000058653 is ".concat(minus)
+ 
+ console.log(text5)
+
+ var pow = Math.pow(6775400000000, 887779000000000000000)
+
+ var text6 = " 6775400000000 to the power of 887779000000000000000 = ".concat(pow)
+
+ console.log(text6)
+
+
+
